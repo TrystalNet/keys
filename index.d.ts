4089100279366bd7c9dd871e4fe5f6fa3da43931
Terry O'Neill <terry.oneill@trystal.net>
2016-07-18 08:29:38 -0600

    experimental

--- index.d.ts
+++ index.d.ts
@@ -1,97 +1,95 @@
-declare module "@trystal/keys" {
-  export const KeyCodes: {
-      UNDEF: number;
-      UP: number;
-      DOWN: number;
-      RIGHT: number;
-      LEFT: number;
-      HOME: number;
-      END: number;
-      DEL: number;
-      ESCAPE: number;
-      ENTER: number;
-      TAB: number;
-      INSERT: number;
-      ZERO: number;
-      ONE: number;
-      TWO: number;
-      THREE: number;
-      FOUR: number;
-      FIVE: number;
-      SIX: number;
-      SEVEN: number;
-      EIGHT: number;
-      NINE: number;
-      A: number;
-      B: number;
-      F: number;
-      H: number;
-      I: number;
-      J: number;
-      K: number;
-      U: number;
-      S: number;
-      PERIOD: number;
-      SPACE: number;
-      CTRL1: number;
-      CTRL2: number;
-      CTRL3: number;
-      CTRL4: number;
-      CTRL5: number;
-      CTRLA: number;
-      CTRLB: number;
-      CTRLC: number;
-      CTRLF: number;
-      CTRLH: number;
-      CTRLL: number;
-      CTRLP: number;
-      CTRLR: number;
-      CTRLS: number;
-      CTRLW: number;
-      CTRLV: number;
-      CTRLY: number;
-      CTRLZ: number;
-      CTRLSPACE: number;
-      CTRLLEFT: number;
-      CTRLRIGHT: number;
-      CTRLENTER: number;
-      CTRLUP: number;
-      CTRLDOWN: number;
-      METALEFT: number;
-      METARIGHT: number;
-      METAUP: number;
-      METADOWN: number;
-      METASPACE: number;
-      METAENTER: number;
-      SHIFTDOWN: number;
-      SHIFTUP: number;
-      CTRLSHIFTC: number;
-      CTRLSHIFTV: number;
-      CTRLESC: number;
-      ALT0: number;
-      F3: number;
-      F5: number;
-      F10: number;
-      F11: number;
-      F12: number;
-      SHIFTF3: number;
-      CTRLF4: number;
-      ALT1: number;
-      ALT2: number;
-      ALT3: number;
-      ALT4: number;
-      ALT5: number;
-      ALTLEFT: number;
-      ALTRIGHT: number;
-      CTRLK: number;
-  };
-  export interface IKeyEvent {
-      altKey: boolean;
-      shiftKey: boolean;
-      ctrlKey: boolean;
-      metaKey: boolean;
-      keyCode: number | undefined;
-      which: number | undefined;
-  }
-  export function keyCodeOf(e: IKeyEvent): number;
+export declare enum KeyCodes {
+    UNDEF = 1,
+    UP = 2,
+    DOWN = 3,
+    RIGHT = 4,
+    LEFT = 5,
+    HOME = 6,
+    END = 7,
+    DEL = 8,
+    ESCAPE = 9,
+    ENTER = 10,
+    TAB = 11,
+    INSERT = 12,
+    ZERO = 13,
+    ONE = 14,
+    TWO = 15,
+    THREE = 16,
+    FOUR = 17,
+    FIVE = 18,
+    SIX = 19,
+    SEVEN = 20,
+    EIGHT = 21,
+    NINE = 22,
+    A = 23,
+    B = 24,
+    F = 25,
+    H = 26,
+    I = 27,
+    J = 28,
+    K = 29,
+    U = 30,
+    S = 31,
+    PERIOD = 32,
+    SPACE = 33,
+    CTRL1 = 35,
+    CTRL2 = 36,
+    CTRL3 = 37,
+    CTRL4 = 38,
+    CTRL5 = 39,
+    CTRLA = 40,
+    CTRLB = 41,
+    CTRLC = 42,
+    CTRLF = 43,
+    CTRLH = 44,
+    CTRLL = 45,
+    CTRLP = 46,
+    CTRLR = 47,
+    CTRLS = 48,
+    CTRLW = 49,
+    CTRLV = 50,
+    CTRLY = 51,
+    CTRLZ = 52,
+    CTRLSPACE = 53,
+    CTRLLEFT = 54,
+    CTRLRIGHT = 55,
+    CTRLENTER = 56,
+    CTRLUP = 57,
+    CTRLDOWN = 58,
+    METALEFT = 59,
+    METARIGHT = 60,
+    METAUP = 61,
+    METADOWN = 62,
+    METASPACE = 63,
+    METAENTER = 64,
+    SHIFTDOWN = 65,
+    SHIFTUP = 66,
+    CTRLSHIFTC = 67,
+    CTRLSHIFTV = 68,
+    CTRLESC = 69,
+    ALT0 = 70,
+    F3 = 1003,
+    F5 = 1005,
+    F10 = 1010,
+    F11 = 1011,
+    F12 = 1012,
+    SHIFTF3 = 1103,
+    CTRLF4 = 1204,
+    ALT1 = 2201,
+    ALT2 = 2202,
+    ALT3 = 2203,
+    ALT4 = 2204,
+    ALT5 = 2205,
+    ALTLEFT = 2206,
+    ALTRIGHT = 2207,
+    CTRLK = 3000,
 }
+export interface IKeyEvent {
+    altKey: boolean;
+    shiftKey: boolean;
+    ctrlKey: boolean;
+    metaKey: boolean;
+    keyCode: number | undefined;
+    which: number | undefined;
+}
+export declare function keyCodeOf(e: IKeyEvent): KeyCodes;
--- index.js
+++ index.js
@@ -1,131 +1,203 @@
 "use strict";
-exports.KeyCodes = {
-    UNDEF: 1, UP: 2, DOWN: 3, RIGHT: 4, LEFT: 5, HOME: 6, END: 7, DEL: 8, ESCAPE: 9, ENTER: 10,
-    TAB: 11, INSERT: 12, ZERO: 13, ONE: 14, TWO: 15, THREE: 16, FOUR: 17, FIVE: 18, SIX: 19, SEVEN: 20,
-    EIGHT: 21, NINE: 22, A: 23, B: 24, F: 25, H: 26, I: 27, J: 28, K: 29,
-    U: 30, S: 31, PERIOD: 32, SPACE: 33, CTRL1: 35, CTRL2: 36, CTRL3: 37, CTRL4: 38, CTRL5: 39, CTRLA: 40,
-    CTRLB: 41, CTRLC: 42, CTRLF: 43, CTRLH: 44, CTRLL: 45, CTRLP: 46, CTRLR: 47, CTRLS: 48, CTRLW: 49, CTRLV: 50,
-    CTRLY: 51, CTRLZ: 52, CTRLSPACE: 53, CTRLLEFT: 54, CTRLRIGHT: 55, CTRLENTER: 56, CTRLUP: 57, CTRLDOWN: 58, METALEFT: 59, METARIGHT: 60,
-    METAUP: 61, METADOWN: 62, METASPACE: 63, METAENTER: 64,
-    SHIFTDOWN: 65, SHIFTUP: 66, CTRLSHIFTC: 67, CTRLSHIFTV: 68, CTRLESC: 69, ALT0: 70,
-    F3: 1003, F5: 1005, F10: 1010, F11: 1011, F12: 1012,
-    SHIFTF3: 1103,
-    CTRLF4: 1204,
-    ALT1: 2201, ALT2: 2202, ALT3: 2203, ALT4: 2204, ALT5: 2205, ALTLEFT: 2206, ALTRIGHT: 2207,
-    CTRLK: 3000
-};
+(function (KeyCodes) {
+    KeyCodes[KeyCodes["UNDEF"] = 1] = "UNDEF";
+    KeyCodes[KeyCodes["UP"] = 2] = "UP";
+    KeyCodes[KeyCodes["DOWN"] = 3] = "DOWN";
+    KeyCodes[KeyCodes["RIGHT"] = 4] = "RIGHT";
+    KeyCodes[KeyCodes["LEFT"] = 5] = "LEFT";
+    KeyCodes[KeyCodes["HOME"] = 6] = "HOME";
+    KeyCodes[KeyCodes["END"] = 7] = "END";
+    KeyCodes[KeyCodes["DEL"] = 8] = "DEL";
+    KeyCodes[KeyCodes["ESCAPE"] = 9] = "ESCAPE";
+    KeyCodes[KeyCodes["ENTER"] = 10] = "ENTER";
+    KeyCodes[KeyCodes["TAB"] = 11] = "TAB";
+    KeyCodes[KeyCodes["INSERT"] = 12] = "INSERT";
+    KeyCodes[KeyCodes["ZERO"] = 13] = "ZERO";
+    KeyCodes[KeyCodes["ONE"] = 14] = "ONE";
+    KeyCodes[KeyCodes["TWO"] = 15] = "TWO";
+    KeyCodes[KeyCodes["THREE"] = 16] = "THREE";
+    KeyCodes[KeyCodes["FOUR"] = 17] = "FOUR";
+    KeyCodes[KeyCodes["FIVE"] = 18] = "FIVE";
+    KeyCodes[KeyCodes["SIX"] = 19] = "SIX";
+    KeyCodes[KeyCodes["SEVEN"] = 20] = "SEVEN";
+    KeyCodes[KeyCodes["EIGHT"] = 21] = "EIGHT";
+    KeyCodes[KeyCodes["NINE"] = 22] = "NINE";
+    KeyCodes[KeyCodes["A"] = 23] = "A";
+    KeyCodes[KeyCodes["B"] = 24] = "B";
+    KeyCodes[KeyCodes["F"] = 25] = "F";
+    KeyCodes[KeyCodes["H"] = 26] = "H";
+    KeyCodes[KeyCodes["I"] = 27] = "I";
+    KeyCodes[KeyCodes["J"] = 28] = "J";
+    KeyCodes[KeyCodes["K"] = 29] = "K";
+    KeyCodes[KeyCodes["U"] = 30] = "U";
+    KeyCodes[KeyCodes["S"] = 31] = "S";
+    KeyCodes[KeyCodes["PERIOD"] = 32] = "PERIOD";
+    KeyCodes[KeyCodes["SPACE"] = 33] = "SPACE";
+    KeyCodes[KeyCodes["CTRL1"] = 35] = "CTRL1";
+    KeyCodes[KeyCodes["CTRL2"] = 36] = "CTRL2";
+    KeyCodes[KeyCodes["CTRL3"] = 37] = "CTRL3";
+    KeyCodes[KeyCodes["CTRL4"] = 38] = "CTRL4";
+    KeyCodes[KeyCodes["CTRL5"] = 39] = "CTRL5";
+    KeyCodes[KeyCodes["CTRLA"] = 40] = "CTRLA";
+    KeyCodes[KeyCodes["CTRLB"] = 41] = "CTRLB";
+    KeyCodes[KeyCodes["CTRLC"] = 42] = "CTRLC";
+    KeyCodes[KeyCodes["CTRLF"] = 43] = "CTRLF";
+    KeyCodes[KeyCodes["CTRLH"] = 44] = "CTRLH";
+    KeyCodes[KeyCodes["CTRLL"] = 45] = "CTRLL";
+    KeyCodes[KeyCodes["CTRLP"] = 46] = "CTRLP";
+    KeyCodes[KeyCodes["CTRLR"] = 47] = "CTRLR";
+    KeyCodes[KeyCodes["CTRLS"] = 48] = "CTRLS";
+    KeyCodes[KeyCodes["CTRLW"] = 49] = "CTRLW";
+    KeyCodes[KeyCodes["CTRLV"] = 50] = "CTRLV";
+    KeyCodes[KeyCodes["CTRLY"] = 51] = "CTRLY";
+    KeyCodes[KeyCodes["CTRLZ"] = 52] = "CTRLZ";
+    KeyCodes[KeyCodes["CTRLSPACE"] = 53] = "CTRLSPACE";
+    KeyCodes[KeyCodes["CTRLLEFT"] = 54] = "CTRLLEFT";
+    KeyCodes[KeyCodes["CTRLRIGHT"] = 55] = "CTRLRIGHT";
+    KeyCodes[KeyCodes["CTRLENTER"] = 56] = "CTRLENTER";
+    KeyCodes[KeyCodes["CTRLUP"] = 57] = "CTRLUP";
+    KeyCodes[KeyCodes["CTRLDOWN"] = 58] = "CTRLDOWN";
+    KeyCodes[KeyCodes["METALEFT"] = 59] = "METALEFT";
+    KeyCodes[KeyCodes["METARIGHT"] = 60] = "METARIGHT";
+    KeyCodes[KeyCodes["METAUP"] = 61] = "METAUP";
+    KeyCodes[KeyCodes["METADOWN"] = 62] = "METADOWN";
+    KeyCodes[KeyCodes["METASPACE"] = 63] = "METASPACE";
+    KeyCodes[KeyCodes["METAENTER"] = 64] = "METAENTER";
+    KeyCodes[KeyCodes["SHIFTDOWN"] = 65] = "SHIFTDOWN";
+    KeyCodes[KeyCodes["SHIFTUP"] = 66] = "SHIFTUP";
+    KeyCodes[KeyCodes["CTRLSHIFTC"] = 67] = "CTRLSHIFTC";
+    KeyCodes[KeyCodes["CTRLSHIFTV"] = 68] = "CTRLSHIFTV";
+    KeyCodes[KeyCodes["CTRLESC"] = 69] = "CTRLESC";
+    KeyCodes[KeyCodes["ALT0"] = 70] = "ALT0";
+    KeyCodes[KeyCodes["F3"] = 1003] = "F3";
+    KeyCodes[KeyCodes["F5"] = 1005] = "F5";
+    KeyCodes[KeyCodes["F10"] = 1010] = "F10";
+    KeyCodes[KeyCodes["F11"] = 1011] = "F11";
+    KeyCodes[KeyCodes["F12"] = 1012] = "F12";
+    KeyCodes[KeyCodes["SHIFTF3"] = 1103] = "SHIFTF3";
+    KeyCodes[KeyCodes["CTRLF4"] = 1204] = "CTRLF4";
+    KeyCodes[KeyCodes["ALT1"] = 2201] = "ALT1";
+    KeyCodes[KeyCodes["ALT2"] = 2202] = "ALT2";
+    KeyCodes[KeyCodes["ALT3"] = 2203] = "ALT3";
+    KeyCodes[KeyCodes["ALT4"] = 2204] = "ALT4";
+    KeyCodes[KeyCodes["ALT5"] = 2205] = "ALT5";
+    KeyCodes[KeyCodes["ALTLEFT"] = 2206] = "ALTLEFT";
+    KeyCodes[KeyCodes["ALTRIGHT"] = 2207] = "ALTRIGHT";
+    KeyCodes[KeyCodes["CTRLK"] = 3000] = "CTRLK";
+})(exports.KeyCodes || (exports.KeyCodes = {}));
+var KeyCodes = exports.KeyCodes;
 function keyCodeOf(e) {
     if (!e)
-        return exports.KeyCodes.UNDEF;
+        return KeyCodes.UNDEF;
     var altKey = e.altKey, shiftKey = e.shiftKey, ctrlKey = e.ctrlKey, metaKey = e.metaKey;
     var which = e.keyCode || e.which || 0;
     if (altKey && ctrlKey)
-        return exports.KeyCodes.UNDEF;
+        return KeyCodes.UNDEF;
     if (altKey && shiftKey)
-        return exports.KeyCodes.UNDEF;
+        return KeyCodes.UNDEF;
     if (metaKey)
         switch (which) {
-            case 37: return exports.KeyCodes.METALEFT;
-            case 38: return exports.KeyCodes.METAUP;
-            case 39: return exports.KeyCodes.METARIGHT;
-            case 40: return exports.KeyCodes.METADOWN;
-            case 13: return exports.KeyCodes.METAENTER;
-            case 32: return exports.KeyCodes.METASPACE;
+            case 37: return KeyCodes.METALEFT;
+            case 38: return KeyCodes.METAUP;
+            case 39: return KeyCodes.METARIGHT;
+            case 40: return KeyCodes.METADOWN;
+            case 13: return KeyCodes.METAENTER;
+            case 32: return KeyCodes.METASPACE;
         }
     if (altKey)
         switch (which) {
-            case 48: return exports.KeyCodes.ALT0;
-            case 49: return exports.KeyCodes.ALT1;
-            case 50: return exports.KeyCodes.ALT2;
-            case 51: return exports.KeyCodes.ALT3;
-            case 52: return exports.KeyCodes.ALT4;
-            case 53: return exports.KeyCodes.ALT5;
-            case 37: return exports.KeyCodes.ALTLEFT;
-            case 39: return exports.KeyCodes.ALTRIGHT;
-            default: return exports.KeyCodes.UNDEF;
+            case 48: return KeyCodes.ALT0;
+            case 49: return KeyCodes.ALT1;
+            case 50: return KeyCodes.ALT2;
+            case 51: return KeyCodes.ALT3;
+            case 52: return KeyCodes.ALT4;
+            case 53: return KeyCodes.ALT5;
+            case 37: return KeyCodes.ALTLEFT;
+            case 39: return KeyCodes.ALTRIGHT;
+            default: return KeyCodes.UNDEF;
         }
     if (shiftKey && ctrlKey)
         switch (which) {
-            case 67: return exports.KeyCodes.CTRLSHIFTC;
-            case 86: return exports.KeyCodes.CTRLSHIFTV;
-            default: return exports.KeyCodes.UNDEF;
+            case 67: return KeyCodes.CTRLSHIFTC;
+            case 86: return KeyCodes.CTRLSHIFTV;
+            default: return KeyCodes.UNDEF;
         }
     if (shiftKey)
         switch (which) {
-            case 38: return exports.KeyCodes.SHIFTUP;
-            case 40: return exports.KeyCodes.SHIFTDOWN;
-            case 114: return exports.KeyCodes.SHIFTF3;
-            default: return exports.KeyCodes.UNDEF;
+            case 38: return KeyCodes.SHIFTUP;
+            case 40: return KeyCodes.SHIFTDOWN;
+            case 114: return KeyCodes.SHIFTF3;
+            default: return KeyCodes.UNDEF;
         }
     if (ctrlKey)
         switch (which) {
-            case 27: return exports.KeyCodes.CTRLESC;
-            case 115: return exports.KeyCodes.CTRLF4;
-            case 32: return exports.KeyCodes.CTRLSPACE;
-            case 49: return exports.KeyCodes.CTRL1;
-            case 50: return exports.KeyCodes.CTRL2;
-            case 51: return exports.KeyCodes.CTRL3;
-            case 52: return exports.KeyCodes.CTRL4;
-            case 53: return exports.KeyCodes.CTRL5;
-            case 37: return exports.KeyCodes.CTRLLEFT;
-            case 39: return exports.KeyCodes.CTRLRIGHT;
-            case 65: return exports.KeyCodes.CTRLA;
-            case 66: return exports.KeyCodes.CTRLB;
-            case 67: return exports.KeyCodes.CTRLC;
-            case 70: return exports.KeyCodes.CTRLF;
-            case 72: return exports.KeyCodes.CTRLH;
-            case 75: return exports.KeyCodes.CTRLK;
-            case 76: return exports.KeyCodes.CTRLL;
-            case 80: return exports.KeyCodes.CTRLP;
-            case 82: return exports.KeyCodes.CTRLR;
-            case 83: return exports.KeyCodes.CTRLS;
-            case 86: return exports.KeyCodes.CTRLV;
-            case 87: return exports.KeyCodes.CTRLW;
-            case 89: return exports.KeyCodes.CTRLY;
-            case 90: return exports.KeyCodes.CTRLZ;
-            case 13: return exports.KeyCodes.CTRLENTER;
-            case 38: return exports.KeyCodes.CTRLUP;
-            case 40: return exports.KeyCodes.CTRLDOWN;
+            case 27: return KeyCodes.CTRLESC;
+            case 115: return KeyCodes.CTRLF4;
+            case 32: return KeyCodes.CTRLSPACE;
+            case 49: return KeyCodes.CTRL1;
+            case 50: return KeyCodes.CTRL2;
+            case 51: return KeyCodes.CTRL3;
+            case 52: return KeyCodes.CTRL4;
+            case 53: return KeyCodes.CTRL5;
+            case 37: return KeyCodes.CTRLLEFT;
+            case 39: return KeyCodes.CTRLRIGHT;
+            case 65: return KeyCodes.CTRLA;
+            case 66: return KeyCodes.CTRLB;
+            case 67: return KeyCodes.CTRLC;
+            case 70: return KeyCodes.CTRLF;
+            case 72: return KeyCodes.CTRLH;
+            case 75: return KeyCodes.CTRLK;
+            case 76: return KeyCodes.CTRLL;
+            case 80: return KeyCodes.CTRLP;
+            case 82: return KeyCodes.CTRLR;
+            case 83: return KeyCodes.CTRLS;
+            case 86: return KeyCodes.CTRLV;
+            case 87: return KeyCodes.CTRLW;
+            case 89: return KeyCodes.CTRLY;
+            case 90: return KeyCodes.CTRLZ;
+            case 13: return KeyCodes.CTRLENTER;
+            case 38: return KeyCodes.CTRLUP;
+            case 40: return KeyCodes.CTRLDOWN;
             default:
                 if (which !== 17)
                     console.log("unknown key combination ctrl+" + which);
-                return exports.KeyCodes.UNDEF;
+                return KeyCodes.UNDEF;
         }
     switch (which) {
-        case 9: return exports.KeyCodes.TAB;
-        case 13: return exports.KeyCodes.ENTER;
-        case 27: return exports.KeyCodes.ESCAPE;
-        case 32: return exports.KeyCodes.SPACE;
-        case 35: return exports.KeyCodes.END;
-        case 36: return exports.KeyCodes.HOME;
-        case 37: return exports.KeyCodes.LEFT;
-        case 38: return exports.KeyCodes.UP;
-        case 39: return exports.KeyCodes.RIGHT;
-        case 40: return exports.KeyCodes.DOWN;
-        case 45: return exports.KeyCodes.INSERT;
-        case 46: return exports.KeyCodes.DEL;
-        case 48: return exports.KeyCodes.ZERO;
-        case 49: return exports.KeyCodes.ONE;
-        case 50: return exports.KeyCodes.TWO;
-        case 51: return exports.KeyCodes.THREE;
-        case 52: return exports.KeyCodes.FOUR;
-        case 53: return exports.KeyCodes.FIVE;
-        case 65: return exports.KeyCodes.A;
-        case 66: return exports.KeyCodes.B;
-        case 70: return exports.KeyCodes.F;
-        case 72: return exports.KeyCodes.H;
-        case 73: return exports.KeyCodes.I;
-        case 74: return exports.KeyCodes.J;
-        case 75: return exports.KeyCodes.K;
-        case 83: return exports.KeyCodes.S;
-        case 85: return exports.KeyCodes.U;
-        case 114: return exports.KeyCodes.F3;
-        case 116: return exports.KeyCodes.F5;
-        case 121: return exports.KeyCodes.F10;
-        case 122: return exports.KeyCodes.F11;
-        case 123: return exports.KeyCodes.F12;
-        case 190: return exports.KeyCodes.PERIOD;
-        default: return exports.KeyCodes.UNDEF;
+        case 9: return KeyCodes.TAB;
+        case 13: return KeyCodes.ENTER;
+        case 27: return KeyCodes.ESCAPE;
+        case 32: return KeyCodes.SPACE;
+        case 35: return KeyCodes.END;
+        case 36: return KeyCodes.HOME;
+        case 37: return KeyCodes.LEFT;
+        case 38: return KeyCodes.UP;
+        case 39: return KeyCodes.RIGHT;
+        case 40: return KeyCodes.DOWN;
+        case 45: return KeyCodes.INSERT;
+        case 46: return KeyCodes.DEL;
+        case 48: return KeyCodes.ZERO;
+        case 49: return KeyCodes.ONE;
+        case 50: return KeyCodes.TWO;
+        case 51: return KeyCodes.THREE;
+        case 52: return KeyCodes.FOUR;
+        case 53: return KeyCodes.FIVE;
+        case 65: return KeyCodes.A;
+        case 66: return KeyCodes.B;
+        case 70: return KeyCodes.F;
+        case 72: return KeyCodes.H;
+        case 73: return KeyCodes.I;
+        case 74: return KeyCodes.J;
+        case 75: return KeyCodes.K;
+        case 83: return KeyCodes.S;
+        case 85: return KeyCodes.U;
+        case 114: return KeyCodes.F3;
+        case 116: return KeyCodes.F5;
+        case 121: return KeyCodes.F10;
+        case 122: return KeyCodes.F11;
+        case 123: return KeyCodes.F12;
+        case 190: return KeyCodes.PERIOD;
+        default: return KeyCodes.UNDEF;
     }
 }
 exports.keyCodeOf = keyCodeOf;
